1a6a997febcf069091cec06daa37949734b869b7
izmahaerill <azmik5671@gmail.com>
2023-08-31 01:06:07 -0400

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Mukta','sans-serif'],
+      serif: ['Poppins','serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
